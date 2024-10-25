62da25035536f38fdaa81499e634bc6be6078880
Sárainé Lajos Imola <lajosimola2@gmail.com>
2024-10-25 22:08:07 +0200

    init

---
+++ src/main/java/hu/magus/pricecalculator/controller/PriceControllerController.java
@@ -0,0 +1,14 @@
+package hu.magus.pricecalculator.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PriceControllerController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "The calculator is working";
+    }
+}
